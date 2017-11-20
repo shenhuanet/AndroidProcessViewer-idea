763a89fdc0787cbda295a7a756da126e05644dcb
shenhuanet <shenhuanet@126.com>
2017-11-20 19:09:16 +0800

    Initial commit

---
+++ src/com/shenhua/idea/plugin/processviewer/etc/Constans.groovy
@@ -0,0 +1,12 @@
+package com.shenhua.idea.plugin.processviewer.etc
+
+/**
+ * Created by shenhua on 2017-11-20-0020.
+ * @author shenhua
+ *         Email shenhuanet@126.com
+ */
+class Constans {
+
+    static final String LEFT_GROUP_ID = "ProcessViewer.LeftGroup"
+    static final String ACTION_ID_REFRESH = "ProcessViewer.RefreshAction"
+}
